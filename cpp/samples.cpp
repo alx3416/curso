4ed7fd8193cb4702ceabb0c936ad5187f0b2bafa
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-08-15 06:48:43 -0600

    creating first cpp files

---
+++ cpp/samples.cpp
@@ -0,0 +1,5 @@
+//
+// Created by victor on 8/15/2024.
+//
+
+#include "samples.h"
---
+++ cpp/samples.h
@@ -0,0 +1,8 @@
+//
+// Created by victor on 8/15/2024.
+//
+
+#ifndef SAMPLES_H
+#define SAMPLES_H
+
+#endif //SAMPLES_H
