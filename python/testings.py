c2980131e818f9dbbfdef8083f75a80d48614d24
Victor Alejandro Gonzalez Huitron <victor.gh@queretaro.tecnm.mx>
2024-08-15 06:37:40 -0600

    adding requirements.txt

--- python/testings.py
+++ python/testings.py
@@ -3,11 +3,11 @@ import samples as samples
 
 
 def test_get_square_area_1():
-    assert ars.get_square_area(4) == 16
+    assert samples.get_square_area(4) == 16
 
 
 def test_get_square_area_2():
-    assert ars.get_square_area(-9.5) == 0
+    assert samples.get_square_area(-9.5) == 0
 
 def test_get_square_area_3():
-    assert ars.get_square_area(0) == 0
+    assert samples.get_square_area(0) == 0
